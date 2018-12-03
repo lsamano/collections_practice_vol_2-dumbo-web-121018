4be3d2f1eda70ba2d2ccb0559425466922712fbd
Leizl Samano <leizl.samano@gmail.com>
2018-12-03 20:36:00 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,20 @@
-# your code goes here
+def begins_with_r(array)
+  array.each do |word|
+    letters = word.chars
+    if letters[0] != "r"
+      return false
+    end
+  end
+  true
+end
+
+def contain_a(array)
+  a_words = []
+  array.each do |word|
+    letters = word.chars
+    if letters.include? == "a"
+      a_words << word
+    end
+  end
+  a_words
+end
